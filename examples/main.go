ed3a9bfbbb70a930601d1c6349656eb0f0a15167
Todd Kulesza <todd@dropline.net>
2025-07-21 22:09:26 -0700

    add example with facets

--- examples/main.go
+++ examples/main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	// BasicExample()
+	BasicExample()
 	ImageExample()
+	ImageAndFacetsExample()
 }
 
 func BasicExample() {
@@ -50,4 +51,25 @@ func ImageExample() {
 	}
 	log.Printf("Post created with URI: %s", uri)
 	log.Println("bsky image example completed")
+}
+
+func ImageAndFacetsExample() {
+	log.Println("bsky image and facets example")
+	server := os.Getenv("BSKY_SERVER")
+	handle := os.Getenv("BSKY_HANDLE")
+	password := os.Getenv("BSKY_PASSWORD")
+	client, err := ltbsky.NewClient(server, handle, password)
+	if err != nil {
+		log.Fatalf("Error creating client: %v", err)
+	}
+	postBuilder := ltbsky.NewPostBuilder("Hello with image and facets! https://go.dev @golang.org")
+	postBuilder.AddLang("en")
+	postBuilder.AddImageFromPath("./test-data/bsky-go-1.png", "Alt text for image")
+
+	uri, err := client.Post(postBuilder)
+	if err != nil {
+		log.Fatalf("Error posting: %v", err)
+	}
+	log.Printf("Post created with URI: %s", uri)
+	log.Println("bsky image and facets example completed")
 }
